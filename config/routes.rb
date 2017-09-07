20afb58a8edeec13d4813f08e47cb8bf2f040479
schiughi <schiughi@gmail.com>
2017-09-07 22:38:37 +0900

    add api    namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'staffs#index'
-  resources :staffs do
-    get :search , on: :collection , defaults: { format: "json" }
+  scope :api, { format: 'json' } do
+    resources :staffs do
+      get :search , on: :collection
+    end
   end
+  resources :staffs
 end
